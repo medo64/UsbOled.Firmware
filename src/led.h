da570e5e2241bce5a8accbf5f9b58d7ee7ea9757
Josip Medved <jmedved@jmedved.com>
2023-09-15 18:47:59 -0700

    Setting LED I/O as open-drain

--- src/led.h
+++ src/led.h
@@ -6,8 +6,8 @@
 
 void led_init();
 
-#define led_activity_off()      LATA4 = 1
-#define led_activity_on()       LATA4 = 0
-#define led_activity_toggle()   LATA4 = !LATA4
+#define led_activity_off()      TRISA4 = 0
+#define led_activity_on()       TRISA4 = 1
+#define led_activity_toggle()   TRISA4 = !TRISA4
 
 #endif
