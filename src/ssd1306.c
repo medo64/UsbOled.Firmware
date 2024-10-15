4bf26e9744f620de582c392a91747fee4f479836
Josip Medved <jmedved@jmedved.com>
2024-10-14 19:41:26 -0700

    Common contrast ratio for all

--- src/ssd1306.c
+++ src/ssd1306.c
@@ -72,15 +72,13 @@ void ssd1306_internalInit() {
     #endif
     if (displayHeight == 32) {
         writeRawCommand2(SSD1306_SET_COM_PINS_HARDWARE_CONFIGURATION, 0x02);      // Set COM Pins Hardware Configuration (0x02 128x32)
-        writeRawCommand2(SSD1306_SET_CONTRAST_CONTROL, 0x8F);                     // Set Contrast Control (0x8F 128x32; external vcc; internal vcc)
     } else if (displayHeight == 128) {
         writeRawCommand2(SSD1306_SET_COM_PINS_HARDWARE_CONFIGURATION, 0x00);      // Set COM Pins Hardware Configuration (0x00 128x128)
-        writeRawCommand2(SSD1306_SET_CONTRAST_CONTROL, 0x53);                     // Set Contrast Control (0x53 128x128; external vcc; internal vcc)
     } else {
         writeRawCommand2(SSD1306_SET_COM_PINS_HARDWARE_CONFIGURATION, 0x12);      // Set COM Pins Hardware Configuration (0x12 128x64)
-        writeRawCommand2(SSD1306_SET_CONTRAST_CONTROL, 0xCF);                     // Set Contrast Control (0xCF 128x64; external vcc; internal vcc)
     }
     writeRawCommand2(SSD1306_SET_MULTIPLEX_RATIO, 0x7F);                          // Needed for 128x128; can be 0x3F for 128x64, or 0x1F for 128x32
+    writeRawCommand2(SSD1306_SET_CONTRAST_CONTROL, 0xDF);                         // Set Contrast Control (0xCF 128x64; external vcc; internal vcc)
     writeRawCommand2(SSD1306_SET_PRECHARGE_PERIOD, 0xF1);                         // Set Pre-Charge Period (0x22 external vcc; 0xF1 internal vcc)
     writeRawCommand2(SSD1306_SET_VCOMH_DESELECT_LEVEL, 0x40);                     // Set VCOMH Deselect Level
     writeRawCommand1(SSD1306_ENTIRE_DISPLAY_ON);                                  // Set Entire Display On/Off
